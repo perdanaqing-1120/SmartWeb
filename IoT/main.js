0de3b4a87a54e31ec8b2f1e29a150d500f86befd
perdanaqing-1120 <perdana.qing@gmail.com>
2025-11-24 20:21:44 +0800

    miku best partner

--- IoT/main.js
+++ IoT/main.js
@@ -1,7 +1,28 @@
+// setup
 const endPoint = "http://192.168.1.29"
+const statusWiFi = document.getElementById("status")
 const kelembapan = document.getElementById("kelembapan")
 const suhu = document.getElementById("suhu")
 
+// status
+setInterval(() => {
+    fetch(`${endPoint}/on`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error();
+            }
+            return response.text()
+        })
+        .then(data => {
+            statusWiFi.value = data
+        })
+        .catch(err => {
+            console.log(err.message);
+            statusWiFi.value = "Connections"
+        })
+}, 2000);
+
+// lampu
 function on() {
     console.log("aktif");
     fetch(`${endPoint}/on`, {
@@ -19,29 +40,34 @@ function off() {
         .then(result => console.log(result))
 }
 
-setInterval(() => {
-    // suhu
-    fetch(`${endPoint}/suhu`, {
-        method: "POST"
-    })
-        .then(response => response.text())
-        .then(result => {
-            if (result > 0) {
-                suhu.value = result
-            }
-        })
-}, 4000);
+// sensor
+let count = false
 
 setInterval(() => {
-    // kelembapan
-    fetch(`${endPoint}/kelembapan`, {
-        method: "POST"
-    })
-        .then(response => response.text())
-        .then(result => {
-            if (result > 0) {
-                kelembapan.value = result
-            }
-        })
+    count = !count
 
-}, 2000)
+    if (count) {
+        // suhu
+        fetch(`${endPoint}/suhu`, {
+            method: "POST"
+        })
+            .then(response => response.text())
+            .then(result => {
+                if (result > 0) {
+                    suhu.value = result
+                }
+            })
+    } else {
+        // kelembapan
+        fetch(`${endPoint}/kelembapan`, {
+            method: "POST"
+        })
+            .then(response => response.text())
+            .then(result => {
+                if (result > 0) {
+                    kelembapan.value = result
+
+                }
+            })
+    }
+}, 2500);
